587c227e82f2f7f5d700ff8057adcbee4bea3fbd
mern4soft <mern4soft@gmail.com>
2024-04-16 11:07:50 +0530

    short term

---
+++ src/SectionFour.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SectionFour() {
+  return (
+    <div>
+      SectionFour
+    </div>
+  )
+}
+
+export default SectionFour
---
+++ src/SectionThree.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SectionOne() {
+  return (
+    <div>
+      SectionOne
+    </div>
+  )
+}
+
+export default SectionOne
