eb1155197555e8e131a53fea7321765e8898df1f
mern4soft <mern4soft@gmail.com>
2024-04-22 09:34:38 +0530

    faq

--- src/index.js
+++ src/index.js
@@ -19,124 +19,6 @@ const App = () => {
 
    return (
       <React.StrictMode>
-         <h1>App</h1>
-
-
-         <div className="flex justify-start bg-black gap-4   items-center">
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">1</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">3</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">2</div>
-         </div>
-
-         <div className="flex justify-center bg-black gap-4  items-center">
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">1</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">3</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">2</div>
-         </div>
-
-         <div className="flex justify-end bg-black gap-4  items-center">
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">1</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">3</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">2</div>
-         </div>
-         <div className="flex justify-between py-10 bg-black gap-4  items-center">
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">1</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">3</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">2</div>
-         </div>
-         <div className="flex justify-evenly bg-black gap-4  items-center">
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">1</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">3</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">2</div>
-         </div>
-         <div className="flex justify-around bg-black gap-4  items-center">
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">1</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">3</div>
-            <div className="bg-green-400 text-white w-10 rounded-full flex justify-center items-center h-10 mb-2">2</div>
-         </div>
-
-
-
-
-         <div className="w-[100%]  gap-0 flex h-auto pb-10  bg-green-400">
-            <div className="h-auto flex-[28%] shadow-xl rounded-md mx-4 bg-white mt-3">
-               <div className="flex justify-between mx-5 items-center mt-2">
-                  <img src="https://static.xx.fbcdn.net/rsrc.php/yb/r/hLRJ1GG_y0J.ico" alt="" />
-                  <img src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" className='w-[200px]' alt="" />
-               </div>
-               <div className=" flex justify-between border mx-5 py-3 px-3 items-center rounded-lg shadow mt-3">
-                  <input type="text" placeholder='Search...' />
-                  <CiSearch />
-               </div>
-
-               <div className="">
-                  <div className="flex transition-all duration-700 hover:text-blue-800 justify-between mx-5  px-3 py-4">
-                     <FaHome size={25} />
-                     <span className='text-[20px] font-medium'>Home</span>
-                  </div>
-                  <div className="flex transition-all duration-700 hover:text-blue-800 justify-between mx-5  px-3 py-4">
-                     <FaHome size={25} />
-                     <span className='text-[20px] font-medium'>Home</span>
-                  </div>
-                  <div className="flex transition-all duration-700 hover:text-blue-800 justify-between mx-5  px-3 py-4">
-                     <FaHome size={25} />
-                     <span className='text-[20px] font-medium'>Home</span>
-                  </div>
-                  <div className="flex transition-all duration-700 hover:text-blue-800 justify-between mx-5  px-3 py-4">
-                     <FaHome size={25} />
-                     <span className='text-[20px] font-medium'>Home</span>
-                  </div>
-                  <div className="flex transition-all duration-700 hover:text-blue-800 justify-between mx-5  px-3 py-4">
-                     <FaHome size={25} />
-                     <span className='text-[20px] font-medium'>Home</span>
-                  </div>
-                  <div className="flex transition-all duration-700 hover:text-blue-800 justify-between mx-5  px-3 py-4">
-                     <FaHome size={25} />
-                     <span className='text-[20px] font-medium'>Home</span>
-                  </div>
-               </div>
-
-               <div className="flex justify-between px-5 items-center">
-                  <IoIosNotifications size={28} />
-                  <span className='text-[20px]'>Notification</span>
-                  <IoMdClose className=' text-white bg-blue-600 rounded-full w-7 h-7 p-1' />
-               </div>
-
-
-               <div className="mb-6 bg-blue-600 justify-between mx-5 rounded-md px-3 py-7 text-white mt-5 flex">
-                  <div className="">
-                     Lorem ipsum dolor
-                  </div>
-                  <p>2</p>
-               </div>
-
-
-               {/* 
-      
-      https://www.uplabs.com/posts/facebook-redesign-ui-challenge
-      
-      */}
-
-            </div>
-            <div className="h-52 flex-[20%] rounded-md mx-4 bg-white mt-3"></div>
-            <div className="h-52 flex-[30%]  rounded-md mx-4 bg-white mt-3"></div>
-         </div>
-         <div className='flex justify-center items-center  h-screen gap-5 '>
-            <div className='mx-5'>
-               <img className='-ms-10 mb-4 h-[10rem]' src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" alt="" />
-               <h1 className='relative bottom-10 text-3xl'>Facebook helps you connect and share with the people in your life.</h1>
-            </div>
-            <div className='shadow-2xl rounded-lg p-10 w-[500px]'>
-               <form action="" className='flex flex-col gap-5'>
-                  <input type="text" placeholder="Email address or Phone Number" className='rounded-[5px] shadow-sm border p-2' />
-                  <input type="text" placeholder="Email address or Phone Number" className='rounded-[5px] shadow-sm   border p-2' />
-                  <button className='bg-blue-600 text-white p-3 rounded-lg'>Log in</button>
-                  <a href="" className='text-center text-blue-600'>forget Password</a>
-                  <hr />
-                  <button className='bg-green-500 text-white text-xl p-3 rounded-lg'>create new Account</button>
-               </form>
-            </div>
-         </div>
 
          <div className='w-[100%] h-[700px]  bg-transparent/100  ' style={{ backgroundImage: "URL('https://assets.nflxext.com/ffe/siteui/vlv3/7ca5b7c7-20aa-42a8-a278-f801b0d65fa1/fb548c0a-8582-43c5-9fba-cd98bf27452f/IN-en-20240326-popsignuptwoweeks-perspective_alpha_website_large.jpg')" }}>
             <nav className='flex justify-around items-center'>
@@ -312,6 +194,9 @@ const App = () => {
             </div>
 
          </section>
+         <footer>
+            
+         </footer>
 
 
 
